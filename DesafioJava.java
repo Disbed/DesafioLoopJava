9fce203abd6cd4500251546d6c63d161f9e8d207
Gabriel Rodrigues <115637109+Disbed@users.noreply.github.com>
2024-04-16 21:31:44 -0300

    Update DesafioJava.java

--- DesafioJava.java
+++ DesafioJava.java
@@ -5,7 +5,7 @@ public class DesafioJava {
         String nomeUsuario = "Luis Carlos Roberto";
         String tipoConta = "Corrente";
         double saldo = 2500;
-        int opcaoMenu;
+        int opcaoMenu = 0;
         double valorReceber;
         double valorTransferir;
         String menu = """
@@ -16,15 +16,7 @@ public class DesafioJava {
                 Tipo conta:        %s  
                 Saldo inicial:     R$%.2f 
                 ************************************************
-                               
-                Operações
-                               
-                1 - Consultar saldos
-                2 - Receber valor
-                3 - Transferir valor
-                4 - Sair
-                               
-                Digite a opção desejada:
+                
                  """.formatted(nomeUsuario, tipoConta, saldo);
         String opcoesMenu = """
                 Operações
@@ -36,40 +28,32 @@ public class DesafioJava {
 
                 Digite a opção desejada:""";
 
-        System.out.println(menu);
-        opcaoMenu = leitura.nextInt();
 
+        System.out.println(menu);
         while (opcaoMenu != 4) {
+            System.out.println(opcoesMenu);
+            opcaoMenu = leitura.nextInt();
             if (opcaoMenu == 1) {
                 System.out.println("Seu saldo na conta é  R$" + saldo);
-                System.out.println(opcoesMenu);
-                opcaoMenu = leitura.nextInt();
             } else if (opcaoMenu == 2) {
                 System.out.println("Digite o valor que deseja receber :");
                 valorReceber = leitura.nextDouble();
                 saldo += valorReceber;
                 System.out.println("Saldo atualizado  R$" + saldo);
-                System.out.println(opcoesMenu);
-                opcaoMenu = leitura.nextInt();
             } else if (opcaoMenu == 3) {
                 System.out.println("Digite o valor que deseja transferir");
                 valorTransferir = leitura.nextDouble();
                 if (valorTransferir <= saldo) {
                     saldo -= valorTransferir;
                     System.out.println("Saldo atualizado  R$" + saldo);
-                    System.out.println(opcoesMenu);
                 } else {
                     System.out.println("Não é possivel transferir esse valor");
-                    System.out.println(opcoesMenu);
                 }
-                opcaoMenu = leitura.nextInt();
-            } else {
+            } else if (opcaoMenu != 4){
                 System.out.println("Opção Invalida!");
-                System.out.println(opcoesMenu);
-                opcaoMenu = leitura.nextInt();
-
 
             }
         }
     }
 }
+
